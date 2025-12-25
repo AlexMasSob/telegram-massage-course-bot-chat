c9abda5dbc4685b59009310baab74bc3d09513b6
AlexMasSob <90padrinho@gmail.com>
2025-12-26 00:46:13 +0200

    Add CallbackQueryHandler import to main.py

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import (
     Application,
     CommandHandler,
+    CallbackQueryHandler,  
     ContextTypes,
     MessageHandler,
     filters,
